944700742711647b5f5a6a669d489d18b3ced078
MatheusMartins-M <127354359+MatheusMartins-M@users.noreply.github.com>
2024-03-18 10:03:55 -0300

    Delete Dev1-Repeticao/teste directory

--- Dev1-Repeticao/teste/3.py
+++
@@ -1,8 +0,0 @@
-soma = 0
-
-for x in range(23, 56):
-    soma += x
-
-    print(x)
-
-print(soma)
--- Dev1-Repeticao/teste/4.py
+++
@@ -1,9 +0,0 @@
-contador = 0
-soma = 0
-
-for x in range (1, 51):
-    if x % 2 == 0:
-        contador += 1
-        soma += x
-
-print(f"Foram utilizados {contador} números para chegar ao resultado {soma}")
